e33bcbe1a624eee21a88f798863e7c8a6473a11d
Patrick <p_nisperos@u.pacific.edu>
2022-03-27 19:07:29 -0700

    removed  --strict on python-app.yml and edited test cases

--- test/test_main.py
+++ test/test_main.py
@@ -1,7 +1,7 @@
 import main
 
 # "test_" must be included for pytest to recognize it as a test case
-def test_add():
+def test_add() -> None:
     assert main.add(3, 4) == 7 # should pass
     assert main.add(4, 4) == 8 # should pass
     
--- test/test_main2.py
+++ test/test_main2.py
@@ -1,6 +1,6 @@
 import main
 
 # "test_" must be included for pytest to recognize it as a test case
-def test_multiply():
+def test_multiply() -> None:
     assert main.multiply(3, 4) == 12 # should pass
     assert main.multiply(4, 4) == 16 # should pass
