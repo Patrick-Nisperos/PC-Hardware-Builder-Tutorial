e4ddb03828378da2fe14ff7751b67b28f64ec412
Patrick <p_nisperos@u.pacific.edu>
2022-03-27 19:00:10 -0700

    edited test cases to show 2 working test cases.

--- test/test_main.py
+++ test/test_main.py
@@ -3,5 +3,5 @@ import main
 # "test_" must be included for pytest to recognize it as a test case
 def test_add():
     assert main.add(3, 4) == 7 # should pass
-    assert main.add(4, 4) == 7 # should fail
+    assert main.add(4, 4) == 8 # should pass
     
