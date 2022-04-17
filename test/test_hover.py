d784b145c2628c72658b3eafe809bf4415d25557
Patrick <p_nisperos@u.pacific.edu>
2022-04-17 12:34:34 -0700

    added -v to pytest, and finished test_hover.py

--- test/test_hover.py
+++ test/test_hover.py
@@ -6,7 +6,20 @@ from python_files import hover
 def test_hoverEnter() -> None:
     # test CPU
     assert hover.hoverEnter("CPU", "mock_label") == "CPU stands for Central Processing Unit. The CPU is the BRAIN of the computer, containing all the circuitry needed to process input, store data, and output results."
-
+    # test GPU
+    assert hover.hoverEnter("GPU", "mock_label") == "GPU stands for Graphics Processing Unit (Graphics Card. The GPU is a specialized electronic circuit that accelerates the creation and rendering of images, video, and animation."
+    # test CPU-cooler
+    assert hover.hoverEnter("CPU-COOLER", "mock_label") == "CPU cooler removes the heat produced from the cpu to keep it from overheating and becoming damaged."
+    # test Antenna
+    assert hover.hoverEnter("ANTENNA", "mock_label") == "These are antenna ports for wifi"
+    # test USB3.2_PS2
+    assert hover.hoverEnter("USB3.2_PS2", "mock_label") == "This is a port for USB3.2 and PS/2"
+    # test LAN_USB2.0
+    assert hover.hoverEnter("LAN_USB2.0", "mock_label") == "This is a port for LAN or local area network ethernet and USB2.0"
+    # test PCIe_x1
+    assert hover.hoverEnter("PCIe_x1", "mock_label") == "PCIe x1 is a slot for low demanding PCIe expansion card"
+    # test PCIe_x16
+    assert hover.hoverEnter("PCIe_x16", "mock_label") == "PCIe x16 is a slot for expansion card with high bandwith requirements like graphics card"
 
 
 """ Test 2 - Test that when mouse doesn't hover, default desc. is displayed """
