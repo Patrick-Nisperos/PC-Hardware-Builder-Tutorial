173c079668084d54dbdd118a01930bb8d1c089ce
Patrick <p_nisperos@u.pacific.edu>
2022-04-21 21:05:49 -0700

    Finished issue Fix multiple check box's bug in quiz mode #44 Finished issue Create quiz mode point system (progress bar) #31

--- python_files/quizMode.py
+++ python_files/quizMode.py
@@ -10,6 +10,7 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+
 class Ui_Quiz(object):
     def contents(self):
         self.questions = ["What is the CPU for computers?", "What is the GPU for computers?"]
@@ -22,6 +23,8 @@ class Ui_Quiz(object):
         self.answer_indexes = [0,1]
 
         self.question_num = 0
+        self.total_question_num = 5
+        self.questions_answered_correct = [] # list of the question numbers (0 to n)
 
     def setupQuiz(self, MainWindow):
         self.contents()
@@ -46,27 +49,12 @@ class Ui_Quiz(object):
         self.title.setStyleSheet("color: rgb(255, 255, 255);")
         self.title.setAlignment(QtCore.Qt.AlignCenter)
         self.title.setObjectName("title")
-        self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
-        self.progressBar.setGeometry(QtCore.QRect(370, 490, 291, 21))
-        self.progressBar.setStyleSheet("color: rgb(255, 255, 255);")
-        self.progressBar.setProperty("value", 24)
-        self.progressBar.setObjectName("progressBar")
-        self.answer_box1 = QtWidgets.QCheckBox(self.centralwidget)
-        self.answer_box1.setGeometry(QtCore.QRect(30, 170, 521, 51))
-        font = QtGui.QFont()
-        font.setPointSize(10)
-        self.answer_box1.setFont(font)
-        self.answer_box1.setStyleSheet("QCheckBox {\n"
-                                        "    spacing: 5px;\n"
-                                        "    color:rgb(255, 255, 255);\n"
-                                        "}\n"
-                                        "QCheckBox::indicator {\n"
-                                        "    width: 40px;\n"
-                                        "}\n"
-                                        "")
-        self.answer_box1.setIconSize(QtCore.QSize(20, 20))
-        self.answer_box1.setChecked(False)
-        self.answer_box1.setObjectName("answer_box1")
+        self.progress_bar = QtWidgets.QProgressBar(self.centralwidget)
+        self.progress_bar.setGeometry(QtCore.QRect(370, 490, 291, 21))
+        self.progress_bar.setStyleSheet("color: rgb(255, 255, 255);")
+        self.progress_bar.setProperty("value", 24)
+        self.progress_bar.setObjectName("progressBar")
+        self.progress_bar.setValue(0)
         self.question_text = QtWidgets.QLabel(self.centralwidget)
         self.question_text.setGeometry(QtCore.QRect(40, 100, 321, 51))
         font = QtGui.QFont()
@@ -120,54 +108,6 @@ class Ui_Quiz(object):
         self.question_status_text.setStyleSheet("color: rgb(0, 255, 0);")
         self.question_status_text.setAlignment(QtCore.Qt.AlignCenter)
         self.question_status_text.setObjectName("question_status_text")
-        self.answer_box2 = QtWidgets.QCheckBox(self.centralwidget)
-        self.answer_box2.setGeometry(QtCore.QRect(30, 220, 521, 51))
-        font = QtGui.QFont()
-        font.setPointSize(10)
-        self.answer_box2.setFont(font)
-        self.answer_box2.setStyleSheet("QCheckBox {\n"
-                                        "    spacing: 5px;\n"
-                                        "    color:rgb(255, 255, 255);\n"
-                                        "}\n"
-                                        "QCheckBox::indicator {\n"
-                                        "    width: 40px;\n"
-                                        "}\n"
-                                        "")
-        self.answer_box2.setIconSize(QtCore.QSize(20, 20))
-        self.answer_box2.setChecked(False)
-        self.answer_box2.setObjectName("answer_box2")
-        self.answer_box3 = QtWidgets.QCheckBox(self.centralwidget)
-        self.answer_box3.setGeometry(QtCore.QRect(30, 270, 521, 51))
-        font = QtGui.QFont()
-        font.setPointSize(10)
-        self.answer_box3.setFont(font)
-        self.answer_box3.setStyleSheet("QCheckBox {\n"
-                                        "    spacing: 5px;\n"
-                                        "    color:rgb(255, 255, 255);\n"
-                                        "}\n"
-                                        "QCheckBox::indicator {\n"
-                                        "    width: 40px;\n"
-                                        "}\n"
-                                        "")
-        self.answer_box3.setIconSize(QtCore.QSize(20, 20))
-        self.answer_box3.setChecked(False)
-        self.answer_box3.setObjectName("answer_box3")
-        self.answer_box4 = QtWidgets.QCheckBox(self.centralwidget)
-        self.answer_box4.setGeometry(QtCore.QRect(30, 320, 521, 51))
-        font = QtGui.QFont()
-        font.setPointSize(10)
-        self.answer_box4.setFont(font)
-        self.answer_box4.setStyleSheet("QCheckBox {\n"
-                                        "    spacing: 5px;\n"
-                                        "    color:rgb(255, 255, 255);\n"
-                                        "}\n"
-                                        "QCheckBox::indicator {\n"
-                                        "    width: 40px;\n"
-                                        "}\n"
-                                        "")
-        self.answer_box4.setIconSize(QtCore.QSize(20, 20))
-        self.answer_box4.setChecked(False)
-        self.answer_box4.setObjectName("answer_box4")
         self.progress_text = QtWidgets.QLabel(self.centralwidget)
         self.progress_text.setGeometry(QtCore.QRect(440, 460, 141, 21))
         font = QtGui.QFont()
@@ -179,6 +119,58 @@ class Ui_Quiz(object):
         self.progress_text.setStyleSheet("color: rgb(255, 255, 255);")
         self.progress_text.setAlignment(QtCore.Qt.AlignCenter)
         self.progress_text.setObjectName("progress_text")
+        self.answer_button1 = QtWidgets.QRadioButton(self.centralwidget)
+        self.answer_button1.setGeometry(QtCore.QRect(30, 180, 521, 51))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setUnderline(False)
+        font.setWeight(50)
+        self.answer_button1.setFont(font)
+        self.answer_button1.setStyleSheet("QRadioButton{color:rgb(255, 255, 255)"
+                                        "}QRadioButton::indicator {width: 40px;"
+                                        "}")
+        self.answer_button1.setObjectName("answer_button1")
+        self.answer_button2 = QtWidgets.QRadioButton(self.centralwidget)
+        self.answer_button2.setGeometry(QtCore.QRect(30, 230, 521, 51))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setUnderline(False)
+        font.setWeight(50)
+        self.answer_button2.setFont(font)
+        self.answer_button2.setStyleSheet("QRadioButton{color:rgb(255, 255, 255)"
+                                        "}QRadioButton::indicator {width: 40px;"
+                                        "}")
+        self.answer_button2.setObjectName("answer_button2")
+        self.answer_button3 = QtWidgets.QRadioButton(self.centralwidget)
+        self.answer_button3.setGeometry(QtCore.QRect(30, 280, 521, 51))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setUnderline(False)
+        font.setWeight(50)
+        self.answer_button3.setFont(font)
+        self.answer_button3.setStyleSheet("QRadioButton{color:rgb(255, 255, 255)"
+                                        "}QRadioButton::indicator {width: 40px;"
+                                        "}")
+        self.answer_button3.setObjectName("answer_button3")
+        self.answer_button4 = QtWidgets.QRadioButton(self.centralwidget)
+        self.answer_button4.setGeometry(QtCore.QRect(30, 330, 521, 51))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setUnderline(False)
+        font.setWeight(50)
+        self.answer_button4.setFont(font)
+        self.answer_button4.setStyleSheet("QRadioButton{color:rgb(255, 255, 255)"
+                                        "}QRadioButton::indicator {width: 40px;"
+                                        "}")
+        self.answer_button4.setObjectName("answer_button4")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 1024, 21))
@@ -187,7 +179,6 @@ class Ui_Quiz(object):
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
-
 
         # Other events called here
         self.set_questions_answers(self.question_num)
@@ -201,22 +192,31 @@ class Ui_Quiz(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.title.setText(_translate("MainWindow", "Hardware Quiz"))
-        self.answer_box1.setText(_translate("MainWindow", "Central Processing Unit - Computes all basic arithmetic and other operations"))
         self.question_text.setText(_translate("MainWindow", "What is the CPU for computers?"))
         self.next_button.setText(_translate("MainWindow", "Next Question"))
         self.previous_button.setText(_translate("MainWindow", "Previous Question"))
         self.question_status_text.setText(_translate("MainWindow", ""))
-        self.answer_box2.setText(_translate("MainWindow", "Computer Power User"))
-        self.answer_box3.setText(_translate("MainWindow", "Critical Patch Update"))
-        self.answer_box4.setText(_translate("MainWindow", "Central Policy Unit"))
+        self.answer_button1.setText(_translate("MainWindow", "Central Processing Unit - Computes all basic arithmetic and other operations"))
+        self.answer_button2.setText(_translate("MainWindow", "Computer Power User"))
+        self.answer_button3.setText(_translate("MainWindow", "Critical Patch Update"))
+        self.answer_button4.setText(_translate("MainWindow", "Central Policy Unit"))
         self.progress_text.setText(_translate("MainWindow", "Quiz Progress"))
+
+    def update_progress_bar(self):
+        value = (float(len(self.questions_answered_correct)) / self.total_question_num) * 100 # Value in percentage
+        self.progress_bar.setValue(value)
 
     def check_answer(self, answer):
         correct_answer = self.answer_indexes[self.question_num]
         if (answer == correct_answer):
             self.question_status_text.setText("Correct!")
             self.question_status_text.setStyleSheet("color: rgb(0, 255, 0);")
-        elif (not self.answer_box1.isChecked() and not self.answer_box2.isChecked() and not self.answer_box3.isChecked() and not self.answer_box4.isChecked()):
+            if (self.question_num in self.questions_answered_correct):
+                return
+            self.questions_answered_correct.append(self.question_num)
+            self.update_progress_bar()
+        elif (not self.answer_button1.isChecked() and not self.answer_button2.isChecked() 
+        and not self.answer_button3.isChecked() and not self.answer_button4.isChecked()):
             self.question_status_text.setText("")
         else:
             self.question_status_text.setText("Incorrect!")
@@ -224,38 +224,43 @@ class Ui_Quiz(object):
 
     def next_question(self):
         self.question_num = self.question_num + 1
+        self.deselect_buttons()
         self.set_questions_answers(self.question_num)
 
     def prev_question(self):
         if (self.question_num > 0):
             self.question_num = self.question_num - 1
             self.set_questions_answers(self.question_num)
+            self.deselect_buttons()
 
     def click_events(self):
-        self.answer_box1.toggled.connect(lambda: self.check_answer(0))
-        self.answer_box2.toggled.connect(lambda: self.check_answer(1))
-        self.answer_box3.toggled.connect(lambda: self.check_answer(2))
-        self.answer_box4.toggled.connect(lambda: self.check_answer(3))
+        self.answer_button1.toggled.connect(lambda: self.check_answer(0))
+        self.answer_button2.toggled.connect(lambda: self.check_answer(1))
+        self.answer_button3.toggled.connect(lambda: self.check_answer(2))
+        self.answer_button4.toggled.connect(lambda: self.check_answer(3))
+
         self.next_button.clicked.connect(lambda: self.next_question())
         self.previous_button.clicked.connect(lambda: self.prev_question())
 
     def set_questions_answers(self, question_num):
-
         self.question_text.setText(self.questions[question_num])
         self.question_status_text.setText("")
         self.question_image.setPixmap(QtGui.QPixmap(self.question_images[question_num]))
 
-        self.answer_box1.setText(self.answers[question_num][0])
-        self.answer_box2.setText(self.answers[question_num][1])
-        self.answer_box3.setText(self.answers[question_num][2])
-        self.answer_box4.setText(self.answers[question_num][3])
-        self.reset_check_boxes()
+        self.answer_button1.setText(self.answers[question_num][0])
+        self.answer_button2.setText(self.answers[question_num][1])
+        self.answer_button3.setText(self.answers[question_num][2])
+        self.answer_button4.setText(self.answers[question_num][3])
 
-    def reset_check_boxes(self):
-        self.answer_box1.setChecked(False)
-        self.answer_box2.setChecked(False)
-        self.answer_box3.setChecked(False)
-        self.answer_box4.setChecked(False)
+    def deselect_buttons(self):
+        # deselect boxes
+        self.answer_buttons = [self.answer_button1, self.answer_button2, self.answer_button3, self.answer_button4]
+        for button in self.answer_buttons:
+            button.setAutoExclusive(False)
+            button.setChecked(False)
+            button.setAutoExclusive(True)
+      
+
 
 
 
